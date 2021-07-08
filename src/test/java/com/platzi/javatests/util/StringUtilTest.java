0193e23c50cb1aa4c3f96d14765911009b496268
gera-garza <gerardo.guillermo.gt@gmail.com>
2021-07-07 23:01:07 -0500

    unit test with the strings

--- src/test/java/com/platzi/javatests/util/StringUtilTest.java
+++ src/test/java/com/platzi/javatests/util/StringUtilTest.java
@@ -3,9 +3,21 @@ package com.platzi.javatests.util;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class StringUtilTest {
+import static org.junit.Assert.assertEquals;
 
-    @Test
+public class StringUtilTest {
+    public static void main(String[] args) {
+
+        String result = StringUtil.repeat("hola",3);
+        assertEquals(result,"holaholahola");
+
+
+        String result2 = StringUtil.repeat("hola",1);
+        if (!result2.equals("hola"))
+            throw new RuntimeException("Error");
+    }
+
+    /*@Test
     public void repeat_string_once() {
         Assert.assertEquals("hola", StringUtil.repeat("hola", 1));
     }
@@ -23,5 +35,5 @@ public class StringUtilTest {
     @Test(expected = IllegalArgumentException.class)
     public void repeat_string_negative_times() {
         StringUtil.repeat("hola", -1);
-    }
+    }*/
 }
